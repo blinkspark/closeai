c578eb90f20fb6ae769ef3064d57f1b8979f6cde
Blinkspark <3829294+blinkspark@users.noreply.github.com>
2025-05-23 10:53:12 +0800

    重构主页和设置页面，移除未使用的代码，添加设置部分和设置项组件

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,62 +1,24 @@
-import 'package:closeai/controllers/provider_controller.dart';
+// import 'package:closeai/controllers/provider_controller.dart';
 import 'package:closeai/pages/home_page/nav_rail.dart';
+import 'package:closeai/pages/setting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/app_state_controller.dart';
-import '../models/provider.dart';
+// import '../models/provider.dart';
 
 class HomePage extends GetResponsiveView<AppStateController> {
   HomePage({super.key});
 
   @override
   Widget builder() {
-    final ProviderController providerController = Get.find();
+    // final ProviderController providerController = Get.find();
     return Scaffold(
       body: Row(
         children: [
           NavRail(),
           VerticalDivider(thickness: 1),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      providerController.addProvider(Provider()..name = 'test');
-                    },
-                    child: Text('Test'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          VerticalDivider(thickness: 1),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Obx(() {
-                return ListView.builder(
-                  itemCount: providerController.providers.length,
-                  itemBuilder: (ctx, index) {
-                    return ListTile(
-                      title: Text(
-                        providerController.providers[index].value.name,
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {
-                          providerController.removeProvider(index);
-                        },
-                        icon: Icon(Icons.delete_rounded),
-                      ),
-                    );
-                  },
-                );
-              }),
-            ),
-          ),
+          Expanded(child: SettingPage()),
         ],
       ),
     );
---
+++ lib/pages/setting_page.dart
@@ -0,0 +1,68 @@
+import 'package:closeai/pages/setting_page/setting_section.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/app_state_controller.dart';
+
+class SettingPage extends GetResponsiveView<AppStateController> {
+  SettingPage({super.key});
+
+  @override
+  Widget builder() {
+    return Row(
+      children: [
+        Container(
+          width: 300,
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Text(
+                '设置',
+                style: Theme.of(Get.context!).textTheme.headlineMedium,
+              ),
+              SizedBox(height: 16),
+              SettingSection(
+                title: '应用',
+                children: [
+                  SettingSectionItem(
+                    title: '主题颜色',
+                    trailing: Obx(() {
+                      return DropdownButton(
+                        items: [
+                          DropdownMenuItem(
+                            value: ThemeMode.light,
+                            child: Text('浅色'),
+                          ),
+                          DropdownMenuItem(
+                            value: ThemeMode.dark,
+                            child: Text('深色'),
+                          ),
+                          DropdownMenuItem(
+                            value: ThemeMode.system,
+                            child: Text('跟随系统'),
+                          ),
+                        ],
+                        underline: Container(),
+                        focusColor: Colors.transparent,
+                        value: controller.themeMode.value,
+                        onChanged: (idx) {
+                          controller.themeMode.value = idx!;
+                        },
+                      );
+                    }),
+                  ),
+                  SettingSectionItem(title: '语言', onPressed: () {}),
+                  SettingSectionItem(
+                    title: '重置',
+                    isDanger: true,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
