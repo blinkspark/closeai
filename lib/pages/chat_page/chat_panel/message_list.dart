7617b4022608d461c6e66f03985e72e59945a193
Blinkspark <3829294+blinkspark@users.noreply.github.com>
2025-05-30 13:40:37 +0800

    feat: 添加链接点击处理，支持外部链接打开

--- lib/pages/chat_page/chat_panel/message_list.dart
+++ lib/pages/chat_page/chat_panel/message_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../controllers/chat_controller.dart';
 
@@ -75,6 +76,16 @@ class MessageWidget extends StatelessWidget {
                               : Theme.of(context).colorScheme.onSurface,
                         ),
                       ),
+                      onTapLink: (text, href, title) async {
+                        if (href != null) {
+                          final uri = Uri.tryParse(href);
+                          if (uri != null) {
+                            if (await canLaunchUrl(uri)) {
+                              await launchUrl(uri, mode: LaunchMode.externalApplication);
+                            }
+                          }
+                        }
+                      },
                     ),
                     if (isStreaming) ...[
                       const SizedBox(height: 8),
